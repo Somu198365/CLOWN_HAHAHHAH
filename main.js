d5424810e2b7efca82f3ee15cc78a70a854f9713
Somu198365 <73246101+Somu198365@users.noreply.github.com>
2021-04-05 20:43:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/yx1fbVhF/co.png');
+}
+noseX=0;
+noseY=0;
+function setup()
+{
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('Pose Net');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x-35;
+        noseY = results[0].pose.nose.y-20;        
+        console.log("nose x = "+noseX);
+        console.log("nose y = "+noseY);
+    }
+}
+function draw(){
+    image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(clown_nose,noseX,noseY,70,50);
+    
+}
+
+function take_snap()
+{
+    save('clown_haha')
+}
